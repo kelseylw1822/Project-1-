c62f488bbf5efd310556fdbf93b858919b993958
kelseylw1822 <kelseylw1822@gmail.com>
2025-01-28 19:01:16 -0500

    start with psuedo code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,52 @@
-//
-//  main.cpp
-//  Project1
-//
-//  Created by Kelsey Wessel on 1/28/25.
-//
+// Kelsey Wessel
+// 1/28/25
+// Problem : Provide students with a self-assessment tool they can use to determine if and for how long they must isolate
 
-#include <iostream>
+// main
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+    // declare variable for Covid test result and date of test
+
+    // read in input if user tested positive or negative
+
+    // if result is positive, then prompt user they must isolate for 7 days from date of test REGARDLESS
+
+    // if result is negative and they were not exposed, then prompt user they do not have to isolate
+
+    // if result is negative and they were exposed:
+
+        // declare two Date class varibles for the date they were exposed and when they recieved their second dose
+    
+        // read in input for variables
+
+        // pass the date they were exposed and date they recieved their second dose to function CalcDays
+
+        // determine studen'ts vaccination status at time of exposure from what was returned
+
+            // if they were fully vacinated by time of exposure, then prompt user that they need to isolate for 3 days from the day of exposure
+            
+            // if they were not fully vacinated by time of exposure, then prompt user they need to isolate for 12 days from day of exposure
+
+// create Date class.
+
+    // create constructor with day, month, year
+
+    // prompt user for input
+    
+    // if input is invalid, constructor should set all variables to default value
+
+
+// create function calcDays that takes in two Date class parameters
+    
+    // declare two varaibles for date1 and date2
+
+    // assign on parameter to date1 and the other to date2
+
+    // calculate the days between 1/01/2023 and date1
+
+    // calculate the days between 1/01/2023 and date2
+
+    // calculate the difference between date1 and date2
+    
+    // return the total amount of days
+
+
