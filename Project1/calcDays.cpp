804705c98e0fb53d0627fc1f546f18335872e946
kelseylw1822 <kelseylw1822@gmail.com>
2025-02-05 12:29:01 -0500

    got project working

--- Project1/calcDays.cpp
+++ Project1/calcDays.cpp
@@ -6,26 +6,51 @@
 // create function calcDays that takes in two Date class parameters and returns an int
 int calcDays(Date dateOne, Date dateTwo)
 {
+    const int monthDays[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    const int leapYear[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
     // declare two objects for date1 and date2
     Date date1, date2;
     // set parameters to date1 and date2
     date1 = dateOne;
     date2 = dateTwo;
     // declare two integer variables for days between the two dates ; daysDiff1 and daysDiff2
-    int daysDiff1, daysDiff2;
+    int daysDiff1 = 0;
+    int daysDiff2 = 0;
     // declare one integer variable for difference between dates ; theDifference
     int theDifference;
     
     // calculate the days between 1/01/2023 and date1, and store in daysDiff1
-    
+    for (int i = 1 ; i < date1.getMonth(); i++)
+    {
+        if (date1.getYear() == 2023)
+        {
+            daysDiff1 += monthDays[i] ;
+        }
+        else
+        {
+            daysDiff1 += leapYear[i] ;
+        }
+    }
+    daysDiff1 += date1.getDay() - 1;
     
     // calculate the days between 1/01/2023 and date2, and store in daysDiff2
+    for (int i = 1 ; i < date2.getMonth(); i++)
+    {
+        if (date2.getYear() == 2023)
+        {
+            daysDiff2 += monthDays[i] ;
+        }
+        else
+        {
+            daysDiff2 += leapYear[i] ;
+        }
+    }
+    daysDiff2 += date2.getDay() - 1;
 
     // assign theDifference with the difference between daysDiff1 and daysDiff2
-    theDifference = daysDiff1 - daysDiff2;
+    theDifference = fabs(daysDiff1 - daysDiff2);
     
     // return the absolute value of theDifference variable
-    //return fabs(theDifference);
-    return 0;
+    return theDifference;
 }
 
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -22,63 +22,80 @@ int main()
     // if result is positive, then prompt user they must isolate for 7 days from date of test REGARDLESS
     if (result == "positive")
     {
-        Date testPositive;
+        string temp;
         cout << "Date tested positive: ";
-  //      cin >> testPositive;
-        cout << "Length of isolation: 7 days";
+        cin >> temp;
+        int month = stoi(temp.substr(0,2));
+        int day = stoi(temp.substr(3,2));
+        int year = stoi(temp.substr(6,4));
+        Date testPositive(day,month,year);
+        cout << "Length of isolation: 7 days" << endl;
         testPositive.addDays(7);
-        cout << "Return to classes: " + testPositive.showDate();
+        cout << "Return to classes: " + testPositive.showDate() << endl;
     }
     
     // if result is negative
-    if (result == "negative")
+    if (result == "negative" || result == "Negative")
     {
         string exposed;
         cout << "Exposed to positive case: ";
         cin >> exposed;
         // if they were not exposed, then prompt user they do not have to isolate
-        if(exposed == "no")
+        if(exposed == "no" || exposed == "No")
         {
-            cout << "Length of isolation: 0 days";
+            cout << "Length of isolation: 0 days" << endl;
         }
         else // if result is negative and they were exposed
         {
-            // declare variables for days between exposure and when recived their second dose
-            int daysBetween;
-            string vacination;
-            // declare two Date class objects for the date they were exposed and when they recieved their second dose
-            Date dateExposed, secondDose;
+            string exposed;
             cout << "Date exposed to positive case: " ;
-    //        cin >> dateExposed;
-            cout << "Second vaccination dose recieved: " ;
+            cin >> exposed;
+            int monthExposed = stoi(exposed.substr(0,2));
+            int dayExposed = stoi(exposed.substr(3,2));
+            int yearExposed = stoi(exposed.substr(6,4));
+            Date dateExposed(dayExposed,monthExposed,yearExposed);
+            
+            string vacination;
+            cout << "Have you gotten your second vaccination dose: " ;
             cin >> vacination;
-            if (vacination == "no")
+            
+            if (vacination == "no" || vacination == "No")
             {
-                cout << "Vaccination status at time of exposure: not fully vaccinated";
-                cout << "Lenght of isolation: 12 days";
+                cout << "Vaccination status at time of exposure: not fully vaccinated"  << endl;
+                cout << "Lenght of isolation: 12 days" << endl;
                 dateExposed.addDays(12);
-                cout << "Return to classes: " + dateExposed.showDate();
+                cout << "Return to classes: " + dateExposed.showDate() << endl;
             }
             else
             {
+                // declare variables for days between exposure and when recived their second dose
+                int daysBetween;
+                
+                string secondVac;
                 cout << "Date second vaccination dose received: ";
-  //              cin >> secondDose;
+                cin >> secondVac;
+                int monthVac = stoi(secondVac.substr(0,2));
+                int dayVac = stoi(secondVac.substr(3,2));
+                int yearVac = stoi(secondVac.substr(6,4));
+                Date secondDose(dayVac,monthVac,yearVac);
+                
                 daysBetween = calcDays(dateExposed, secondDose);
+                
                 // if they were not fully vacinated by time of exposure, then prompt user they need to isolate for 12 days from day of exposure
                 if (daysBetween < 14)
                 {
-                    cout << "Vaccination status at time of exposure: not fully vaccinated";
-                    cout << "Length of isolation: 12 days";
+                    cout << "Vaccination status at time of exposure: not fully vaccinated" << endl;
+                    cout << "Length of isolation: 12 days" << endl;
                     dateExposed.addDays(12);
-                    cout << "Return to classes: " + dateExposed.showDate();
+                    cout << "Return to classes: " + dateExposed.showDate() << endl;
                 }
                 // if they were fully vacinated by time of exposure, prompt user that they need to isolate for 3 days from the day of exposure
                 else
                 {
-                    cout << "Vaccination status at time of exposure: fully vaccinated";
-                    cout << "Length of isolation: 3 days";
+                    cout << "Vaccination status at time of exposure: fully vaccinated" << endl;
+                    cout << "Length of isolation: 3 days" << endl;
                     dateExposed.addDays(3);
-                    cout << "Return to classes: " + dateExposed.showDate();
+                    cout << "Return to classes: " + dateExposed.showDate() << endl;
                 }
             }
         }
